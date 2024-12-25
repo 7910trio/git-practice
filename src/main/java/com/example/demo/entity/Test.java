08985326592ea96b5e847ae70466c9459a3cd271
7910trio <7910trio@naver.com>
2024-12-25 16:09:16 +0900

    :init: init: create project

---
+++ src/main/java/com/example/demo/entity/Test.java
@@ -0,0 +1,12 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+public class Test {
+    String name;
+    int score;
+}
